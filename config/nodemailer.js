020fc2a39b93d2d220a6626a184136f2e92bae1d
Rishabh Jha <rishabhjha0@gmail.com>
2024-10-26 12:25:24 +0530

    fix node mailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,8 +2,8 @@ const nodemailer=require('nodemailer')
 let transporter = nodemailer.createTransport({
     service: 'gmail', // You can use other email services too
     auth: {
-      user: 'letzbattle.tech@gmail.com', // Your email address
-      pass: ''   // Your email password or app password
+      user: 'nexgenbattles.tech@gmail.com', // Your email address
+      pass: process.env.EMAIL_PASS  // Your email password or app password
     }
   });
   module.exports={
